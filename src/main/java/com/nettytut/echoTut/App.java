73a60299a420a6e9c90089402f5ea732a08554e8
dmitriy <demar.07@gmail.com>
2016-10-27 14:28:07 +0900

    Initial commit

---
+++ src/main/java/com/nettytut/echoTut/App.java
@@ -0,0 +1,13 @@
+package com.nettytut.echoTut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
