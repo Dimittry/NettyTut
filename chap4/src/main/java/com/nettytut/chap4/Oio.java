6be5d7f1e2d6701d7bc2c6b5fc49d814016b5349
olyver <demar.07@gmail.com>
2016-10-30 01:55:48 +0900

    Just code from book.

---
+++ chap4/src/main/java/com/nettytut/chap4/Oio.java
@@ -0,0 +1,48 @@
+package com.nettytut.chap4;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class Oio {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Oio");
+        (new Oio()).serve(9999);
+    }
+
+    public void serve(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for(;;) {
+                final Socket clientSocket = socket.accept();
+                System.out.println("Accepted connection from "
+                        + clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\r\n".getBytes(
+                                    Charset.forName("UTF-8")
+                            ));
+                            out.flush();
+                        } catch(IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException e) {
+
+                            }
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
